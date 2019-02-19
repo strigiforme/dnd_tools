fb5d6d61ebb1148456d6ab90d9eb7b961ea584df
Howard <howardpearce0@gmail.com>
2019-02-19 16:43:28 -0400

    Added some fun colors

--- dnd_home.php
+++ dnd_home.php
@@ -12,35 +12,40 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] = TRUE){
       <h1 class = "primary-font banner-title"> D&D Tools </h1>
     </div>
     <div id = "content-panel" class = "flex column p-10">
-      <p class =  "bubble-title">  Characters: </p>
-      <div class = "bubble">
-        <p class = "bubble-data"> No Characters yet! </p>
+      <div id = "characters">
+        <p class =  "bubble-title">  Characters: </p>
+        <div class = "bubble">
+          <p class = "bubble-data"> No Characters yet! </p>
+        </div>
       </div>
-      <br/>
-      <p class = "bubble-title">  Campaigns: </p>
-      <div class = "bubble">
-        <p class = "bubble-data"> No Campaigns yet! </p>
+      <div id = "campaigns">
+        <p class = "bubble-title">  Campaigns: </p>
+        <div class = "bubble">
+          <p class = "bubble-data"> No Campaigns yet! </p>
+        </div>
       </div>
-      <br/>
-      <p class = "bubble-title"> Homebrew: </p>
-      <div class = "bubble">
-        <p class = "bubble-data"> No Homebrew yet! </p>
+      <div id = "homebrew">
+        <p class = "bubble-title"> Homebrew: </p>
+        <div class = "bubble">
+          <p class = "bubble-data"> No Homebrew yet! </p>
+        </div>
       </div>
-      <br/>
-      <p class = "bubble-title"> Users: </p>
-      <?php
-      // make query to get all Users
-      $userQuery = "SELECT login_username, login_id from dnd_login";
-      $userQueryResult = $conn->query($userQuery);
+      <div id = "users">
+        <p class = "bubble-title"> Users: </p>
+        <?php
+        // make query to get all Users
+        $userQuery = "SELECT login_username, login_id from dnd_login";
+        $userQueryResult = $conn->query($userQuery);
 
-      if($userQueryResult){
-        while($row = $userQueryResult->fetch_assoc()){
-            echo "<div class = 'bubble'>";
-            echo "<p class = 'bubble-data'>" . $row['login_username'] . "</p>";
-            echo "</div>";
+        if($userQueryResult){
+          while($row = $userQueryResult->fetch_assoc()){
+              echo "<div class = 'bubble'>";
+              echo "<p class = 'bubble-data'>" . $row['login_username'] . "</p>";
+              echo "</div>";
+          }
         }
-      }
-      ?>
+        ?>
+      </div>
     </div>
     <div class = "col-2-3 m-left-10 home p-10">
       <h2 class = "mainbody-header"> Notifications </h2>
